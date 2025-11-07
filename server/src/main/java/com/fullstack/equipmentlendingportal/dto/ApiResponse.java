bd94ff8f828f892a7ee343f4eb1becc810167548
Saravanan06 <59788540+Saravanan06@users.noreply.github.com>
2025-11-07 11:08:26 +0530

    Create ApiResponse.java
    To get a APIResponse for Equipment Dashboard

---
+++ server/src/main/java/com/fullstack/equipmentlendingportal/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.fullstack.equipmentlendingportal.equipment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+	
+	private String status;
+	private String message;
+	private T data;
+
+}
